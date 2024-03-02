89ba6400eca4ea6779b9d671357f00b9bbb9e967
aldolares238 <aldo225918@gmail.com>
2024-03-01 23:48:28 -0600

    Metodo round() y datos flotantes

--- 008_Float_Redondeo-float_V0.py
+++ 008_Float_Redondeo-float_V0.py
@@ -6,10 +6,8 @@
 #Almacenamos en una variable flotante un numero con muchos decimales
 num=17.567383292929200234
 #Imprimimos esa variable usando el metodo round para mostrar solamente 5 decimales redondeados, ademas se utilizan conocimientos varios para dar otro resultado
-dato=(18.45245/47)**5
+dato=(18.45245/47)**5 #Operamos utiliando ** para exponetes
 print("El numero 17.567383292929200234 redondeado a 5 decimales es: ",round(num,5),"\n\nAhora hacemos una operacion con flotante y exponencial, para redondear el resultado a dos decimales: (18.45245/47)**5= ",round(dato,2),"\n\n\nPronto todo acabara")
-
-
 
 
 
