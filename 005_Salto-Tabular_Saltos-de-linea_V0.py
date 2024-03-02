2570ec41c1ef89262ad84859aadb07dc1dddcc40
aldolares238 <aldo225918@gmail.com>
2024-03-01 23:40:22 -0600

    Tabulaciones y saltos de linea

--- 005_Salto-Tabular_Saltos-de-linea_V0.py
+++ 005_Salto-Tabular_Saltos-de-linea_V0.py
@@ -4,7 +4,7 @@
 #Practica: 5_Salto Implementar en un solo string una lista de palabras, cada palabra debe tener su propia linea
 
 #Se hace un print en el que entre cada palabra se da un salto de linea y una tabulacion para que el texto se muestre a manera de lista
-print("Lenguajes de programacion:\n\t-Python\n\t-JavaScript\n\t-Java\n\t-PHP\n\t-TypeScript\n\t-SQL\n\t-COBOL\n\t-HTML\n\t-CSS")
+print("Lenguajes de programacion:\n\t-Python\n\t-JavaScript\n\t-Java\n\t-PHP\n\t-TypeScript\n\t-SQL\n\t-COBOL\n\t-HTML\n\t-CSS") #Imprimimos
 
-#Usamos el concepto de otra manera
+#Usamos el concepto de otra manera, manejando saltos de linea y tabulaciones para acomodar el texto a preferencia
 print("Y siempre que nazcas de nuevo cantras esto a tu gente y dijo:\n\tDebes cantar como si nadie te estuviese escuchando\n\tDebes bailar como si nadie te estuviese observando\n\tDebes amar sin miedo a ser traicionado\n\tAunque sin darle prioridad al que prioridad no te ha dado, claro")
