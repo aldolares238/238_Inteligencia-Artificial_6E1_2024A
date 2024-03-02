c3a6a2b8582052d3fd2459e2d2e926a5de2bd72f
aldolares238 <aldo225918@gmail.com>
2024-03-01 23:44:11 -0600

    Operaciones basicas

--- 006_Operaciones_SumResMultDiv_V0.py
+++ 006_Operaciones_SumResMultDiv_V0.py
@@ -3,12 +3,10 @@
 #Grupo: 6E1
 #Practica: 7_Operaciones Utilizar los operadores basicos para resolver operaciones en diferentes variables
 #Se realizan las operaciones solicitadas, una para cada variable
-suma=20+23+30+14
-resta=-20-23-30-14
-multi=(20*23)+(92*5)
-div=(5000/230)/(5/10)
-operacion=10/5+15-17
+suma=20+23+30+14 #Operacion suma
+resta=-20-23-30-14 #Operacion resta
+multi=(20*23)+(92*5)#Operaciondivision
+div=(5000/230)/(5/10)#Operacion multiplicacion
+operacion=10/5+15-17#Operacion combinada
 #Para mostrar los resultados se utiliza una misma instruccion print()
 print("Resultados:\n\t-Suma (20+23+30+14) = ",suma,"\n\t-Resta (-20-23-30-14)",resta,"\n\t-Multiplicacion ((20*23)+(92*5)) = ",multi,"\n\t-Division ((5000/230)/(5/10)) = ",div,"\n\t-Operacion (10/5+15-17) = ",operacion)
-
-
